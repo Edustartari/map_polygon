881121c53726cb8a168fd02d99c41b741dfc36ba
Edustartari <edustartari@gmail.com>
2026-04-08 00:03:06 -0300

    update cronjob config deploy script 8

--- build.sh
+++ build.sh
@@ -18,26 +18,4 @@ cd ..
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-# Echo the current OS used in the build process
-echo "Current OS: $(uname -a)"
-# Echo the current linux distribution
-if [ -f /etc/os-release ]; then
-    . /etc/os-release
-    echo "Linux Distribution: $NAME $VERSION"
-else
-    echo "Linux distribution information not found."
-fi
-
-# Set the cronjob to run every minute
-dnf update -y && dnf install -y cronie
-# Start crond daemon in background
-crond &
-# Get the absolute path of the cron job script
-cronjob_path="$(pwd)/cron_job.sh"
-chmod +x "$cronjob_path"
-echo "Made $cronjob_path executable"
-(crontab -l 2>/dev/null; echo "* * * * * $cronjob_path") | crontab -
-echo "Crontab updated successfully"
-crontab -l
-
 echo "BUILD END"
