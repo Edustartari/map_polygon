92ec3593f7f976d4bd8cc1a148e185e970314670
Edustartari <edustartari@gmail.com>
2026-04-07 23:46:25 -0300

    update cronjob config deploy script 6

--- build.sh
+++ build.sh
@@ -36,7 +36,9 @@ crond &
 cronjob_path="$(pwd)/cron_job.sh"
 chmod +x "$cronjob_path"
 echo "Made $cronjob_path executable"
-(crontab -l 2>/dev/null; echo "* * * * * $cronjob_path") | crontab -
+crontab -e <<EOF
+* * * * * $cronjob_path
+EOF
 echo "Crontab updated successfully"
 crontab -l
 
